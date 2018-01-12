1b332e3df8775ca5fa7f17b6c843c98c55df05a2
Luan Nico <luanpotter27@gmail.com>
2018-01-12 09:58:50 -0200

    gae does not play way with log4j

--- src/main/java/xyz/luan/dextra/parking/ws/FirebaseAuthFilter.java
+++ src/main/java/xyz/luan/dextra/parking/ws/FirebaseAuthFilter.java
@@ -6,41 +6,42 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseToken;
 import io.yawp.commons.http.HttpException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Objects;
 import java.util.concurrent.ExecutionException;
+import java.util.logging.Logger;
 
 public class FirebaseAuthFilter extends HttpFilter {
 
-    private Logger LOGGER = LoggerFactory.getLogger(FirebaseAuthFilter.class);
+	private final static Logger LOGGER = Logger.getLogger(FirebaseAuthFilter.class.getName());
 
-    @Override
-    protected void filter(HttpServletRequest request, HttpServletResponse response) throws ExecutionException, InterruptedException {
-        String authToken = request.getHeader("Authorization");
-        LOGGER.info("Request received [url: {}][auth: {}]", request.getRequestURI(), authToken);
+	@Override
+	protected void filter(HttpServletRequest request, HttpServletResponse response) throws ExecutionException, InterruptedException {
+		String authToken = request.getHeader("Authorization");
+		LOGGER.info(String.format("Request received [url: %s][auth: %s]", request.getRequestURI(), authToken));
 
-        if (Objects.nonNull(authToken)) {
-            String idToken = authToken.split(" ")[1];
-            FirebaseToken decodedToken = getFirebase().verifyIdTokenAsync(idToken).get();
-            if (!decodedToken.isEmailVerified()) {
-                throw new HttpException(403, "You can only login with a verified e-mail!");
-            }
-            AuthHolder.email.set(decodedToken.getEmail());
-        }
-    }
+		if (Objects.nonNull(authToken)) {
+			String idToken = authToken.split(" ")[1];
+			FirebaseToken decodedToken = getFirebase().verifyIdTokenAsync(idToken).get();
+			if (!decodedToken.isEmailVerified()) {
+				throw new HttpException(403, "You can only login with a verified e-mail!");
+			}
+			String email = decodedToken.getEmail();
+			LOGGER.info(String.format("Authenticated: [%s]", email));
+			AuthHolder.email.set(email);
+		}
+	}
 
-    private static FirebaseAuth _auth;
+	private static FirebaseAuth _auth;
 
-    private static FirebaseAuth getFirebase() {
-        if (_auth == null) {
-            FirebaseOptions options = new FirebaseOptions.Builder().setProjectId("dextra-parking").setCredentials(GoogleCredentials.newBuilder().build()).build();
-            FirebaseApp app = FirebaseApp.initializeApp(options);
-            _auth = FirebaseAuth.getInstance(app);
-        }
-        return _auth;
-    }
+	private static FirebaseAuth getFirebase() {
+		if (_auth == null) {
+			FirebaseOptions options = new FirebaseOptions.Builder().setProjectId("dextra-parking").setCredentials(GoogleCredentials.newBuilder().build()).build();
+			FirebaseApp app = FirebaseApp.initializeApp(options);
+			_auth = FirebaseAuth.getInstance(app);
+		}
+		return _auth;
+	}
 }
